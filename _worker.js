e51c0485ff78efb7af689923c4761caa122ebbb7
Roseberry <165325276+sahar-km@users.noreply.github.com>
2024-04-20 15:30:39 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = '302682d9-b512-4595-b9b9-b9ed836d69f9';
 
 
-const proxyIPs = ['pxy.revil.onflashdrive.app'];
+const proxyIPs = ['152.69.178.16'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://dns.adguard-dns.com/dns-query'; // OR use https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg= OR https://8.8.8.8/dns-query OR https://cloudflare-dns.com/dns-query
