4d4d444d382e4934fbd02624f0f91bf9a1287cc3
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-04-28 20:26:13 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'cf943265-445a-4c81-9c2d-ffa4a4a16354';
 
 
-const proxyIPs = ['ipdb.rr.nu'];
+const proxyIPs = ['50.114.177.206'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://melbourne-k0ypv0z4bjr2.deno.dev/1:AKA4vAAg'; // OR use  https://dns.adguard-dns.com/dns-query  OR https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg= OR https://8.8.8.8/dns-query OR https://cloudflare-dns.com/dns-query
