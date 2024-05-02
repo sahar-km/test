91cb03da8149c0a51aa9207a0446508ab835d048
Roseberry <165325276+sahar-km@users.noreply.github.com>
2024-05-02 05:54:54 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'cf943265-445a-4c81-9c2d-ffa4a4a16354';
 
 
-const proxyIPs = ['130.61.23.77'];
+const proxyIPs = ['5.29.194.1'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://dns.adguard-dns.com/dns-query'; // OR use. https://melbourne-k0ypv0z4bjr2.deno.dev/1:AKA4vAAg. OR  https://dns.adguard-dns.com/dns-query  OR https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg= OR https://8.8.8.8/dns-query OR https://cloudflare-dns.com/dns-query
